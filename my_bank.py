39b6a09f6a4f7d6b2d2fa9c025ae07752eae76f4
SaynaSarhangian <sayna.sarhangian1@gmail.com>
2023-10-23 08:07:24 +1100

    Complete class for Bank, Customer + some test cases

--- my_bank.py
+++ my_bank.py
@@ -1,46 +1,5 @@
-from typing import List
-
-
-class Customer:
-
-    def __init__(self, account_id: int):
-        self.account_id = account_id
-        self.balance = 0
-
-    def withdraw(self, withdraw_amount: int):
-        if withdraw_amount > self.balance:
-            print(f'Abort! withdraw_amount {withdraw_amount} cannot be greater than balance {self.balance}')
-            raise Exception('Invalid amount!')
-        else:
-            self.balance -= withdraw_amount
-            print(
-                f'success! ${withdraw_amount} was withdrawn from your account. your balance now is ${self.balance}')
-
-    def deposit(self, deposit_amount: int):
-        self.balance += deposit_amount
-
-    def get_balance(self):
-        return self.balance
-
-
-class Bank:
-    def __init__(self, bank_balance: int = 0):
-        self.bank_balance = bank_balance
-        self.customers: List[Customer] = []
-
-    def add_customer(self, customer_obj: Customer):
-        self.customers.append(customer_obj)
-
-    def get_bank_balance(self):
-        return self.bank_balance
-
-    def deposit_operation(self, id, amount):
-        self.bank_balance += amount
-        for item in self.customers:
-            if item.account_id == id:
-                item.deposit(amount)
-
-
+from bank import Bank
+from customer import Customer
 
 if __name__ == '__main__':
     # cus1.deposit(2600)
--- test_bank.py
+++ test_bank.py
@@ -1,10 +1,19 @@
 import unittest
-
-from Bank.my_bank_revised import Customer
+from bank import Bank
+from customer import Customer
 
 
 class TestBank(unittest.TestCase):
-    def test_create_customer(self):
-        new_customer = Customer(110)
-        assert new_customer is not None
+    def test_add_customer(self):
+        b1 = Bank()
+        c1 = Customer(110)
+        b1.add_customer(c1)
+        assert b1.customers[0] == c1
 
+    def test_deposit_operation(self):
+        """test with existing customer"""
+        b1 = Bank()
+        c1 = Customer(110)
+        b1.add_customer(c1)
+        b1.deposit_operation(110, 1000)
+        self.assertEqual(b1.bank_balance, 1000)
